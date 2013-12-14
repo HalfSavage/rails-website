b6f8c3b3fb3bf39ebfe600ed86e973f1c3938b9d
booty <johnedmundrose@gmail.com>
2013-12-14 13:56:28 -0500

    Updated models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :replies, class_name: 'Post', foreign_key: 'reply_to_post_id'
-  belongs_to :parent, class_name: 'Member'
+  belongs_to :parent, class_name: 'Post'
   belongs_to :member
   has_many :forums_posts
   has_many :forums, -> { uniq },  through: :forums_posts  # see for explanation of uniq: http://stackoverflow.com/questions/16569994/deprecation-warning-when-using-has-many-through-uniq-in-rails-4
@@ -8,11 +8,13 @@ class Post < ActiveRecord::Base
 
   after_initialize :set_defaults, on: [:create]
 
-  validates :subject, length: {minimum: 5, maximum: 200}
-  validates :body, length: {minimum: 10, maximum: 4000}
+  # TODO: Validate subject length BUT only if parent post (replies don't need subjects)
+  # validates :subject, length: {minimum: 5, maximum: 200}
+  validates :body, length: {minimum: 10, maximum: 8000}
   validates :member, presence: true
   # validates :parent, presence: true  # if post is an OP, it is its own parent
 
+  scope :threads, -> { where(parent_id: nil) }
 
   def set_defaults
     self.is_deleted = false if (self.is_deleted.nil?)
