995a1a5e088c29e163089a605304807275a480dc
booty <johnedmundrose@gmail.com>
2014-02-16 21:58:41 -0500

    Added views and shit to schema, thanks to schema_plus gem

--- db/schema.rb
+++ db/schema.rb
@@ -17,14 +17,6 @@ ActiveRecord::Schema.define(version: 20140216233625) do
   enable_extension "plpgsql"
   enable_extension "pg_stat_statements"
 
-  create_table "forum_moderators", force: true do |t|
-    t.string   "forum_moderators"
-    t.integer  "forum_id"
-    t.integer  "member_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "forums", force: true do |t|
     t.string   "name"
     t.boolean  "is_active"
@@ -41,25 +33,8 @@ ActiveRecord::Schema.define(version: 20140216233625) do
   create_table "forums_posts", force: true do |t|
     t.integer "forum_id", null: false
     t.integer "post_id",  null: false
+    t.index ["forum_id", "post_id"], :name => "by_forum_and_post", :unique => true
   end
-
-  add_index "forums_posts", ["forum_id", "post_id"], name: "by_forum_and_post", unique: true, using: :btree
-
-  create_table "genders", force: true do |t|
-    t.string "gender_description"
-    t.string "gender_abbreviation"
-  end
-
-  create_table "likes", force: true do |t|
-    t.integer  "member_id"
-    t.integer  "post_id"
-    t.integer  "attachment_id"
-    t.integer  "event_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "likes", ["post_id", "member_id"], name: "index_likes_on_post_id_and_member_id", unique: true, using: :btree
 
   create_table "members", force: true do |t|
     t.string   "username"
@@ -87,11 +62,51 @@ ActiveRecord::Schema.define(version: 20140216233625) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "gender_id"
+    t.index ["email"], :name => "index_members_on_email", :unique => true
+    t.index ["reset_password_token"], :name => "index_members_on_reset_password_token", :unique => true
+    t.index ["username"], :name => "index_members_on_username", :unique => true
   end
 
-  add_index "members", ["email"], name: "index_members_on_email", unique: true, using: :btree
-  add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
-  add_index "members", ["username"], name: "index_members_on_username", unique: true, using: :btree
+  create_table "posts", force: true do |t|
+    t.integer  "member_id"
+    t.integer  "parent_id"
+    t.boolean  "is_deleted"
+    t.boolean  "is_public_moderator_voice"
+    t.boolean  "is_private_moderator_voice"
+    t.text     "body"
+    t.datetime "marked_as_answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "subject"
+    t.integer  "thread_id"
+    t.string   "slug"
+    t.index ["parent_id", "created_at", "member_id"], :name => "ix_posts_for_threads", :order => {"parent_id" => :asc, "created_at" => :desc, "member_id" => :asc}
+    t.index ["slug"], :name => "index_posts_on_slug", :unique => true
+  end
+
+  create_view "discussions", " SELECT f.name, \n    fp.forum_id, \n    p.id, \n    p.member_id, \n    p.body, \n    p.subject, \n    p.created_at, \n    p.updated_at, \n    m.username, \n    ( SELECT count(1) AS count\n           FROM posts preply\n          WHERE ((preply.parent_id = p.id) AND (preply.is_deleted = false))) AS reply_count, \n    plastreply.reply_created_at, \n    plastreply.reply_username\n   FROM ((((posts p\n   JOIN forums_posts fp ON ((p.id = fp.post_id)))\n   JOIN forums f ON ((fp.forum_id = f.id)))\n   JOIN members m ON ((p.member_id = m.id)))\n   LEFT JOIN ( SELECT rank() OVER (PARTITION BY p_1.parent_id ORDER BY p_1.created_at DESC) AS reply_number, \n    p_1.parent_id, \n    p_1.created_at AS reply_created_at, \n    m_1.username AS reply_username, \n    m_1.id AS reply_member_id\n   FROM (posts p_1\n   JOIN members m_1 ON ((p_1.member_id = m_1.id)))\n  WHERE ((p_1.parent_id IS NOT NULL) AND (p_1.is_deleted IS NOT NULL))) plastreply ON (((p.id = plastreply.parent_id) AND (plastreply.reply_number = 1))))\n  WHERE ((p.parent_id IS NULL) AND (p.is_deleted = false))\n  ORDER BY COALESCE(plastreply.reply_created_at, p.created_at) DESC", :force => true
+  create_table "forum_moderators", force: true do |t|
+    t.string   "forum_moderators"
+    t.integer  "forum_id"
+    t.integer  "member_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genders", force: true do |t|
+    t.string "gender_description"
+    t.string "gender_abbreviation"
+  end
+
+  create_table "likes", force: true do |t|
+    t.integer  "member_id"
+    t.integer  "post_id"
+    t.integer  "attachment_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["post_id", "member_id"], :name => "index_likes_on_post_id_and_member_id", :unique => true
+  end
 
   create_table "message_types", force: true do |t|
     t.string "name"
@@ -109,11 +124,11 @@ ActiveRecord::Schema.define(version: 20140216233625) do
     t.datetime "deleted_by_sender"
     t.datetime "deleted_by_recipient"
     t.boolean  "is_moderator_voice"
+    t.index ["member_to_id", "message_type_id"], :name => "index_messages_on_member_to_id_and_message_type_id"
+    t.index ["member_to_id", "seen", "message_type_id"], :name => "index_messages_on_member_to_id_and_seen_and_message_type_id"
   end
 
-  add_index "messages", ["member_to_id", "message_type_id"], name: "index_messages_on_member_to_id_and_message_type_id", using: :btree
-  add_index "messages", ["member_to_id", "seen", "message_type_id"], name: "index_messages_on_member_to_id_and_seen_and_message_type_id", using: :btree
-
+  create_view "pg_stat_statements", " SELECT pg_stat_statements.userid, \n    pg_stat_statements.dbid, \n    pg_stat_statements.query, \n    pg_stat_statements.calls, \n    pg_stat_statements.total_time, \n    pg_stat_statements.rows, \n    pg_stat_statements.shared_blks_hit, \n    pg_stat_statements.shared_blks_read, \n    pg_stat_statements.shared_blks_dirtied, \n    pg_stat_statements.shared_blks_written, \n    pg_stat_statements.local_blks_hit, \n    pg_stat_statements.local_blks_read, \n    pg_stat_statements.local_blks_dirtied, \n    pg_stat_statements.local_blks_written, \n    pg_stat_statements.temp_blks_read, \n    pg_stat_statements.temp_blks_written, \n    pg_stat_statements.blk_read_time, \n    pg_stat_statements.blk_write_time\n   FROM pg_stat_statements() pg_stat_statements(userid, dbid, query, calls, total_time, rows, shared_blks_hit, shared_blks_read, shared_blks_dirtied, shared_blks_written, local_blks_hit, local_blks_read, local_blks_dirtied, local_blks_written, temp_blks_read, temp_blks_written, blk_read_time, blk_write_time)", :force => true
   create_table "post_action_types", force: true do |t|
     t.string   "name"
     t.boolean  "moderator_only"
@@ -130,9 +145,8 @@ ActiveRecord::Schema.define(version: 20140216233625) do
     t.datetime "updated_at"
     t.integer  "post_id"
     t.string   "note"
+    t.index ["post_id", "post_action_type_id", "created_at"], :name => "post_actions_post_id_etc"
   end
-
-  add_index "post_actions", ["post_id", "post_action_type_id", "created_at"], name: "post_actions_post_id_etc", using: :btree
 
   create_table "post_tags", id: false, force: true do |t|
     t.integer  "post_id",    null: false
@@ -141,28 +155,11 @@ ActiveRecord::Schema.define(version: 20140216233625) do
     t.datetime "updated_at"
   end
 
-  create_table "posts", force: true do |t|
-    t.integer  "member_id"
-    t.integer  "parent_id"
-    t.boolean  "is_deleted"
-    t.boolean  "is_public_moderator_voice"
-    t.boolean  "is_private_moderator_voice"
-    t.text     "body"
-    t.datetime "marked_as_answer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "subject"
-    t.integer  "thread_id"
-    t.string   "slug"
-  end
-
-  add_index "posts", ["parent_id", "created_at", "member_id"], name: "ix_posts_for_threads", order: {"created_at"=>:desc}, using: :btree
-  add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
-
   create_table "tags", force: true do |t|
     t.string   "tag_text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_view "tags_trending", " SELECT pt.tag_id, \n    t.tag_text, \n    count(pt.tag_id) AS count, \n    fp.forum_id, \n    sum(\n        CASE\n            WHEN ((date_part('epoch'::text, now()) - date_part('epoch'::text, pt.created_at)) < (7200)::double precision) THEN (100.0)::double precision\n            ELSE ((1)::double precision + ((100.0)::double precision / ((date_part('epoch'::text, now()) - date_part('epoch'::text, pt.created_at)) / (7200.0)::double precision)))\n        END) AS score\n   FROM ((post_tags pt\n   JOIN forums_posts fp ON ((pt.post_id = fp.post_id)))\n   JOIN tags t ON ((pt.tag_id = t.id)))\n  GROUP BY fp.forum_id, pt.tag_id, t.tag_text\n  ORDER BY sum(\nCASE\n    WHEN ((date_part('epoch'::text, now()) - date_part('epoch'::text, pt.created_at)) < (7200)::double precision) THEN (100.0)::double precision\n    ELSE ((1)::double precision + ((100.0)::double precision / ((date_part('epoch'::text, now()) - date_part('epoch'::text, pt.created_at)) / (7200.0)::double precision)))\nEND) DESC\n LIMIT 100", :force => true
 end
