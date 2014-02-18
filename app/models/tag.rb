3c8173eeae7da5ef04941d651481a89d9a8d8b70
booty <johnedmundrose@gmail.com>
2014-02-18 12:30:11 -0500

    Functionality of "TagTrendingByForum" (yuck) moved into Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,11 @@
 class Tag < ActiveRecord::Base
   # TODO: add relation to PostTag and Post (through PostTag)
 
+  def self.trending_in_forum(f)
+    fid = (f.is_a? Forum) ? f.id : f.to_i 
+    joins("inner join tags_trending_by_forum ttbf on ttbf.tag_id = tags.id and ttbf.forum_id = #{fid}")
+  end 
+
   def self.find_by_tag_or_new(tag_text)
     tag = Tag.where('lower(tag_text)=lower(?)',tag_text.downcase).first
     if tag then 
--- app/models/tag_trending_by_forum.rb
+++
@@ -1,7 +0,0 @@
-class TagTrendingByForum < ActiveRecord::Base
-  # It maps to a view, actually. Magical!
-  self.table_name = "tags_trending_by_forum"
-  self.primary_key = :tag_id
-
-  belongs_to :forum 
-end
