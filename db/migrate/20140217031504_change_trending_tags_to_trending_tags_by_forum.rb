47d647fd9017b5267ec1d29907e0bb275e81fc4f
booty <johnedmundrose@gmail.com>
2014-03-09 23:32:37 -0400

    Not necessary; removed

--- db/migrate/20140217031504_change_trending_tags_to_trending_tags_by_forum.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTrendingTagsToTrendingTagsByForum < ActiveRecord::Migration
-  def self.up
-    execute 'alter view tags_trending rename to tags_trending_by_forum;'
-  end
-
-  def self.down
-    execute 'alter view tags_trending_by_forum rename to tags_trending;'
-  end
-end
