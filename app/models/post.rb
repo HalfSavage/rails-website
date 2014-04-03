8f9f7d2d9be65436ec2f088e4b09eab85ba3d882
booty <johnedmundrose@gmail.com>
2014-04-03 17:35:12 -0400

    Improved validation of permissions, tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,14 +12,27 @@ class Post < ActiveRecord::Base
   # TODO: add relation to PostTag and Tag (through PostTag)
 
   # Hooks
-  before_save :create_slug
+  before_validation :create_slug, if: :post_is_parent_of_discussion?
   before_save :check_if_tags_need_to_be_deleted
   after_save :update_tags
 
-  # TODO: Validate subject length BUT only if parent post (replies don't need subjects)
-  # validates :subject, length: {minimum: 5, maximum: 200}
+  # Validations that always happen
+  # TODO: Validate duplicate posts by a single member
   validates :body, length: {minimum: 10, maximum: 8000}
   validates :member, presence: true
+  validate :member_has_permission_to_post_in_forum
+
+  # Validations that apply only if this post is the parent of a new discussion (a.k.a. "thread")
+  validates :subject, length: {minimum: 5, maximum: 200}, if: :post_is_parent_of_discussion?
+  validates_presence_of :forums_posts, if: :post_is_parent_of_discussion?, message: "...discussion must belong to some forums"
+  validates_presence_of :subject, if: :post_is_parent_of_discussion?
+  validates_presence_of :slug, if: :post_is_parent_of_discussion?
+
+  # Validations that apply only if this post isn't the parent of discussion
+  # Should we cover these possibilities with tests instead of validations?
+  validates_absence_of :forums_posts, unless: :post_is_parent_of_discussion?
+  validates_absence_of :subject, unless: :post_is_parent_of_discussion?
+  validates_absence_of :slug, unless: :post_is_parent_of_discussion?
 
   # Scopes
   scope :discussions, -> { where(parent_id: nil) }
@@ -28,8 +41,10 @@ class Post < ActiveRecord::Base
   # that we can put in the URL so that we can have pretty, Google-able URLs
   # like http://halfsavage.com/discussions/why-does-my-poop-smell or whatever
   def create_slug 
-    self.slug = subject.parameterize if (self.subject && self.slug.nil?)
-    while Post.where("slug=?",self.slug).any? do
+    self.slug = subject.parameterize if (self.subject.present? && self.slug.blank?)
+    # Ensure uniqueness of slug; this could probably be less ugly!
+    # debugger
+    while Post.where("slug=? and id<>coalesce(?,-1)", self.slug, self.id).any? do
       self.slug += ('-' + rand(0..100).to_s)
     end 
   end 
@@ -43,45 +58,83 @@ class Post < ActiveRecord::Base
     # to just delete the tags that are no longer in the post. But this only happens when a post
     # is updated, not during the first time it's saved, so no biggie
 
-    #puts "update_tags: new record? #{@was_a_new_record}"
     if !@was_a_new_record 
-      #puts "need to delete PostTags..."
       PostTag.delete_all(post_id: id) 
     end 
 
     # Pull all the hashtags out into array via regex
     tags = body.scan(/(?:(?<=\s)|^)#(\w*[A-Za-z_]+\w*)/)
-    #debugger
+  
     # We need a unique, case-insensitive list of tags
     # tags.uniq! would be case-sensitive but luckly .uniq and .uniq! can accept a block
     tags.flatten!
     tags.uniq!{ |elem| elem.downcase }  
   
-    # todo: this should be a configurable setting somewhere.
     # We'll only parse the first max_tags_per_post tags because unlike a single tweet,
     # you could stuff a truly obnoxious number of tags into a single post
-    max_tags_per_post = 10
-    
-    # There is probably a much more Ruby-ish way of doing this loop :)
-    count=0
-    tags.each{ |tag_text| 
-      if (count<max_tags_per_post) then
-        tag = Tag.find_by_tag_or_new(tag_text)
-        tag.save! if tag.new_record?
-        PostTag.create!({tag: tag, post: self, created_at: self.created_at})
-      end 
+    tags.take(MAX_TAGS_PER_POST).each{ |tag_text| 
+      tag = Tag.find_by_tag_or_new(tag_text)
+      tag.save! if tag.new_record?
+      PostTag.create!({tag: tag, post: self, created_at: self.created_at})
     }
   end 
 
-  def is_thread?
+  def post_is_parent_of_discussion?
     parent.nil?
   end 
 
+  def post_is_not_parent_of_discussion?
+    !post_is_parent_of_discussion?
+  end 
+
   def in_public_forum? 
-    return parent.in_public_forum if parent 
-    forums.each { |f| 
-      return true if !f.is_moderator_only
-    }
+    return parent.in_public_forum? if parent 
+    in_at_least_one_public_forum?
+  end 
+
+  # Is the post in at least a single non-moderator forum?
+  # Posts/discussions in multiple forums can be replied to by non-mods, 
+  # as long as the post is in at least one non-moderator forum
+  def in_at_least_one_public_forum?
+    return parent.in_public_forum? if parent 
+    forums.each { |f| return true if !f.moderator_only? }
     false
   end 
+
+  def only_in_moderator_forums?
+    return !in_at_least_one_public_forum?
+  end 
+
+  def member_has_permission_to_post_in_forum
+    # is member banned?
+    errors[:base] << "Banned members can't post in forums" if member.banned?
+
+    # is member active?
+    errors[:base] << "Inactive members can't post in forums" if member.inactive?
+
+    if self.post_is_parent_of_discussion? 
+      the_forums = forums 
+    else 
+      the_forums = parent.forums 
+    end 
+    errors[:forums] << "Post... seems to have no forums? Whaaa? Huh..." if the_forums.blank?
+
+    # if forum is paid-only, is member paid?
+    # note: if discussion exists in multiple forums, user only needs permission for one of them!
+    has_permission = false 
+    the_forums.each do |f| 
+      has_permission = true if !f.paid_member_only? || member.paid?
+    end 
+    errors[:base] << "Only paid members may post in this forum" if !has_permission 
+
+    # if forum is mod-only, is member mod?
+    # note: if discussion exists in multiple forums, user only needs permission for one of them!
+    # todo: should refactor to use only_in_moderator_forum? above
+    has_permission = false 
+    the_forums.each do |f|
+      has_permission = true if !f.moderator_only || member.moderator?
+    end 
+    errors[:base] << "Only moderators may post in this forum" if !has_permission 
+  end 
+
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,127 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not allow post by banned member" do
+    p = Post.new({
+      member: members(:banned_member),
+      parent: posts(:thread_one),
+      body: 'Lorem ipsum dolor'
+      })
+
+    assert_not p.save
+  end 
+
+  test "should allow post by unbanned member" do 
+    p = Post.new({
+      member: members(:beth),
+      parent: posts(:thread_one),
+      body: 'Lorem ipsum dolor'
+      })
+
+    assert p.save
+  end 
+
+  test "should not allow post by inactive member" do
+    p = Post.new({
+      member: members(:inactive_member),
+      parent: posts(:thread_one),
+      body: 'Lorem ipsum dolor'
+      })
+
+    assert_not p.save
+  end 
+
+  test "should allow post by active member" do 
+    p = Post.new({
+      member: Member.where(active: true).first,
+      parent: posts(:thread_one),
+      body: 'Lorem ipsum dolor'
+      })
+
+    assert p.save
+  end 
+
+  test "should not allow non-mod to create thread in the mod forum" do
+    p = Post.new({
+      member: members(:carrie),
+      body: 'Lorem ipsum dolor',
+      subject: 'This is the subject',
+      forums: [forums(:mod_forum)]
+      })
+    assert_not p.save
+  end
+
+  test "should allow non-mod to reply to thread that exists in mod AND non-mod forums" do 
+    p = Post.new({
+      member: members(:carrie),
+      body: 'Lorem ipsum dolor',
+      parent: posts(:mod_and_public_forum_thread)
+      })
+    assert p.save
+  end 
+
+  test "should not allow non-mod to reply to thread that only exists in mod forum" do 
+    p = Post.new({
+      member: members(:carrie),
+      body: 'Lorem ipsum dolor',
+      parent: posts(:mod_forum_thread)
+      })
+    assert_not p.save
+  end 
+
+  test "shouldn't allow non-parent post to have forums association" do
+    p = Post.new({
+      member: members(:manny_moderator),
+      parent: posts(:thread_one),
+      body: 'Lorem ipsum dolor',
+      forums: [forums(:public_forum)]
+      })
+
+    assert_not p.save, "that's just weird..."
+  end
+
+  test "should allow mod to post to the mod forum" do
+    p = Post.new({
+      member: members(:manny_moderator),
+      body: 'Lorem ipsum dolor',
+      subject: 'This is the subject',
+      forums: [forums(:mod_forum)]
+      })
+    assert p.save
+  end  
+
+  test "post should create tags correctly" do
+    
+    p = Post.new({
+      member: members(:manny_moderator),
+      body: 'Lorem ipsum dolor #aaaa #bbbb #cccc',
+      subject: 'This is the subject',
+      forums: [forums(:mod_forum)]
+      })
+    assert p.save
+
+    assert PostTag.where(post: p).count == 3
+
+  end 
+
+  test "post should not create too many tags in the case of tag spam" do
+    p = Post.new({
+      member: members(:manny_moderator),
+      body: 'Lorem ipsum dolor #aaaa #bbbb #cccc #wvsd #fvsdf #vdfs #vsdfvdsfvdsfv #sdfv #dsfv #sdfvsvjknreuyvn #5enavs #cvag983g #avnfkjn  #vdfkv #dofgbnonbg #b53b3b #gbgny #vdfvbs #rbsb #srbtvdv #drhfsd #dfvsdfs #dfvs #sfdvd #qqq #www #eee #rrr #ttt #yyy #uuu #iii #ooo #pppp #aaa #ss #ddd #fff #ggg #hhh #jjj',
+      subject: 'This is the subject',
+      forums: [forums(:mod_forum)]
+      })
+    assert p.save
+
+    assert PostTag.where(post: p).count <= MAX_TAGS_PER_POST, "Created too many tags - #{PostTag.where(post: p).count} instead of the max of #{MAX_TAGS_PER_POST}"
+
+  end 
+
+  test "post should not allow unpaid member to post to the paid forum" do 
+  end 
+
+  test "post should allow paid member to post to the paid forum" do
+  end
+
 end
