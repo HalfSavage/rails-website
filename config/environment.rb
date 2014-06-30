4b2c34e2a636f965f7772e518bccc9dbffb0a57e
booty <johnedmundrose@gmail.com>
2014-06-29 21:24:12 -0400

    Should start storing config in here

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Halfsavage::Application.initialize!
+
+# TODO: Should configuration information really go into this file?
+Rails.configuration.forum_threads_per_page = 20
+Rails.configuration.discussion_slug_length = 50
