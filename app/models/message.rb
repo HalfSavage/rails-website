da5408e2fa00d8b25c7401d9882d4caae8787ebf
booty <johnedmundrose@gmail.com>
2014-07-22 08:47:39 -0400

    Initial stab at Message model, text, fixtures

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,71 @@
 class Message < ActiveRecord::Base
-	belongs_to :member_from, class_name: 'Member', foreign_key: 'member_to_id'
-  belongs_to :member_to, class_name: 'Member', foreign_key: 'member_from_id'
+	belongs_to :member_from, class_name: 'Member', foreign_key: 'member_from_id'
+  belongs_to :member_to, class_name: 'Member', foreign_key: 'member_to_id'
   belongs_to :message_type
   belongs_to :post
 
   validates :member_from, presence: true
   validates :member_to, presence: true 
   validates :message_type, presence: true
+  validate :sender_has_permission_to_send?
+
+  attr_accessor :viewing_member 
+
+  def obscure_content? 
+    return nil if @viewing_member.nil? # We don't know who's viewing, so we can't say
+    return false if moderator_voice?
+    @viewing_member.message_content_should_be_obscured?
+  end 
+
+  # Excludes deleted messages
+  def self.conversations_for_member(member, viewing_member = nil)
+    member_id = (member.is_a? Member) ? member.id : member 
+    viewing_member = viewing_member || ((member.is_a? Member) ? member : Member.find(member_id))
+
+    messages = Message.find_by_sql ["select * from conversations 
+      where 
+        (member_to_id=:member_id or member_from_id=:member_id) 
+        and ((member_to_id=:member_id and deleted_by_recipient is null) or (member_from_id=:member_id and deleted_by_sender is null))
+        and conversation_message_number <=3
+      order by conversation_number, conversation_message_number", member_id: member_id]
+    messages.each { |m| m.viewing_member = viewing_member } 
+  end 
+
+  def self.conversation_for_members(member, other_member)
+    member_id = (member.is_a? Member) ? member.id : member 
+    other_member_id = (other_member.is_a? Member) ? other_member.id : other_member
+    
+    messages = conversation_for_members_include_deleted(member_id, other_member_id)
+    messages.where("((member_to_id=:member and deleted_by_recipient is null) or (member_from_id=:other_member_id and deleted_by_sender is null))")
+    messages.each { |m| m.viewing_member = member } 
+  end 
+
+  def self.conversation_for_members_include_deleted(member, other_member)
+    member_id = (member.is_a? Member) ? member.id : member 
+    other_member_id = (other_member.is_a? Member) ? other_member.id : other_member
+
+    Message.where("(member_to_id in (:member_id,:other_member_id) and member_from_id in (:member_id,:other_member_id))",
+      member_id: member_id,
+      other_member_id: other_member_id
+      ).order(created_at: :desc)
+  end 
+
+  def self.find_for_member(member, id)
+    member_id = (member.is_a? Member) ? member.id : member 
+
+    Message.where("(member_to_id=:member_id or member_from_id=:member_id) 
+        and ((member_to_id=:member_id and deleted_by_recipient is null) or (member_from_id=:member_id and deleted_by_sender is null))
+        and id = :id", member_id: member_id, id: id)
+  end 
+
+  def not_moderator_voice?
+    !moderator_voice?
+  end 
+
+  def sender_has_permission_to_send?
+    return true if !new_record?
+    return false if member_from.nil?
+    member_from.permission_to_send_message?
+  end 
+
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -4,4 +4,78 @@ class MessageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should return messages for user, grouped by conversation" do 
+    messages = Message.conversations_for_member(members(:albert).id)
+    assert_equal messages.count, 6, "Wrong number of convos! Is #{messages.count}, should be 6"
+  end 
+
+  test "should not return message deleted by sender when sender retrieves it" do 
+    messages = Message.conversations_for_member(members(:beth)) 
+    messages.each { |message|
+      assert_not_equal message, messages(:message_deleted_by_sender), "Deleted message shouldn't show up in Beth's conversations"
+    }
+  end 
+
+  test "should not return message deleted by recip when recip retrieves it" do 
+    messages = Message.conversations_for_member(members(:edgar)) 
+    messages.each { |message|
+      assert_not_equal message, messages(:message_deleted_by_recipient), "Deleted message shouldn't show up in Edgar's conversations"
+    }
+  end 
+
+  test "should return a specific message for the sender" do
+    assert Message.find_for_member(members(:carrie), messages(:ten)).present?, "Couldn't find Carrie->Albert msg for Carrie"
+  end 
+
+  test "should return a specific message for the recipient" do 
+    assert Message.find_for_member(members(:albert), messages(:ten)).present?, "Couldn't find Carrie->Albert msg for Albert"
+  end 
+
+  test "should not return a specific message for a user that is neither sender nor recipient" do 
+    assert Message.find_for_member(members(:frida), messages(:ten)).empty?, "Couldn't find Carrie->Albert msg for Albert"
+  end 
+
+  test "unpaid member's messages should be obscured, unless they're from in moderator voice" do
+    messages = Message.conversations_for_member(members(:unpaid_guy))
+    ug = members(:unpaid_guy)
+    puts "unpaid_guy: obscure? #{ug.message_content_should_be_obscured?} paid? #{ug.paid?}"
+    messages.each do |message| 
+      if (!message.moderator_voice?) 
+        assert message.obscure_content?, "Content should've been obscured"
+      end 
+      if message.moderator_voice?
+        assert_not message.obscure_content?, "Content shouldn't have been obscured, it's mod voice" 
+      end 
+    end      
+    
+  end
+
+  test "paid member's messages should not be obscured" do 
+    messages.each do |message| 
+      assert_not message.obscure_content?, "Content shouldn't have been obscured, viewing member is paid" 
+    end   
+
+  end 
+
+  test "unpaid member shouldn't be able to read message unless it's a moderator voice message" do  
+  end
+
+  test "non-mod shouldn't be able to send message in moderator voice" do 
+  end 
+
+  test "mod should be able to send message in moderator voice" do 
+  end 
+
+  test "banned member shouldn't be able to send message" do 
+  end 
+
+  test "banned moderator shouldn't be able to send message" do 
+  end     
+
+  test "paid member shouldn't be able to message self" do 
+  end 
+
+  test "paid member shouldn't be able to send message to nil user" do 
+  end 
 end
