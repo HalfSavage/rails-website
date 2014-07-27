0549498d711178e09d23450195ace2cc6b7a4435
booty <johnedmundrose@gmail.com>
2014-07-27 18:28:46 -0400

    Custom flash tpes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  add_flash_types :error, :success, :warning, :danger
+
   protected
 
   def configure_permitted_parameters
-    #devise_parameter_sanitizer.for(:sign_up) << :username << :first_name << :gender_id
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-
   end
 end
