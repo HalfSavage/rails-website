f688f1b2ecf3677cedac0498c5d85b386f81371b
booty <johnedmundrose@gmail.com>
2014-09-08 16:18:32 -0400

    Reversed the clauses, wasn't showing first name

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -6,7 +6,7 @@ module MemberHelper
       }.merge(options)
       
     if member.is_a? Member 
-      display_name = options[:include_first_name] ? member.username : member.username_and_firstname
+      display_name = options[:include_first_name] ? member.username_and_firstname : member.username
       return "<a href=\"members/#{member.username_slug}\" class=\"member_link\"><i class=\"fa fa-user\"></i> #{display_name}</a>".html_safe
     else
       return "<a href=\"members/#{member.username_slug}\" class=\"member_link\"><i class=\"fa fa-user\"></i> #{member}</a>".html_safe
